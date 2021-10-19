34d2c3242846f78488b205d6f7828b555b671751
Roger <rgrang816@west-mec.org>
2021-10-18 18:23:39 -0700

    added audio synth to side bar

---
+++ audio.js
@@ -0,0 +1,15 @@
+window.AudioContext = window.AudioContext || window.webkitAudioContext;
+context = new AudioContext();
+var osc = context.createOscillator();
+osc.frequency.value = 440;
+osc.type = "sawtooth";
+osc.connect(context.destination);
+
+function startosc(val){
+    osc = context.createOscillator();
+osc.frequency.value = val;
+osc.type = "square";
+osc.connect(context.destination);
+osc.start(0)
+}
+function stoposc(){osc.stop(0);}
